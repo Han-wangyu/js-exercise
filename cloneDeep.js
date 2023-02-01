1608af83aeb236c3d23badc64ab2c843fb99327c
hanwangyu <hanwangyu@foxmail.com>
2023-02-02 00:12:37 +0800

    cloneDeep

---
+++ cloneDeep.js
@@ -0,0 +1,25 @@
+function cloneDeep(target, map = new WeakMap()) {
+    if (typeof target === 'object') {
+        let cloneTarget = Array.isArray(target) ? [] : {};
+        if (map.get(target)) return map.get(target);
+        map.set(target, cloneTarget);
+        for (const key in target) {
+            cloneTarget[key] = cloneDeep(target[key], map);
+        }
+        return cloneTarget;
+    }
+    return target;
+}
+
+// testing
+const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8]
+};
+target.target = target;
+console.log(cloneDeep(target));
+console.log(structuredClone(target));
